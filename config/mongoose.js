01c6d38b31c26015343e937d27f30ceb63189934
Ashvarya <94183306+Ashvarya@users.noreply.github.com>
2023-09-19 16:39:58 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const url = 'mongodb://127.0.0.1/issue_Tracker';
+const url = 'mongodb://localhost/issue_Tracker';
 // mongoose.connect('url') 
 mongoose.connect(url);
 const db=mongoose.connection;

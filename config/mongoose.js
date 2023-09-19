0a6acb9f239e454c49fc92237d70dc8a371ee293
Ashvarya <ashvaryak2552@gmail.com>
2023-09-19 16:47:51 +0530

    new

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const url = 'mongodb://127.0.0.1/issue_Tracker';
+const url = 'mongodb+srv://ashvaryak2552:issue-tracker@cluster0.qvkdhhr.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp';
 // mongoose.connect('url') 
 mongoose.connect(url);
 const db=mongoose.connection;
